b06da9167ee99c7aadcabfec617d59fe4a5a5683
mcube3567 <mcube3567@gmail.com>
2020-01-01 21:37:16 +0100

    npm

---
+++ npm.js
@@ -0,0 +1,18 @@
+//written codes by a community
+//search for packages in d npm registry
+//packages contain 2 tings 1. a js file 2.package  json file
+//package.json file is a minifile dat describes d package
+ /*setting up npm and package json
+ node allos us run javascript outside d browser
+ npm -v // npm version
+ node -v //node version
+ npm init //initializes npm
+ react file lives in npm
+ live-server
+ installing it -g
+ npm install -g live-server
+ sudo npm install -g live-server
+ lodash
+ npm install lodash
+ npm install browseriy
+ */
